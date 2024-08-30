72af2f4c88b8d38a6de7ba318fcf7459d6927df0
zarah <devvick230385@gmail.com>
2024-08-30 16:55:14 +0100

    UPDATE: resolved verbosity

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -3,21 +3,21 @@ pragma solidity ^0.8.24;
 
 library Name {
 
-    address yo = chiii({a:7,self:hex"Hello"});
-    //
-//     address user = address   (address(0xa).arg().arch(1)).toString().toAddr();
-//      bytes user = bytes16(bytes32(0xa).toString(1000_000)).toBytes(16,user).oi();
-//      bytes bts = bytes(0x1);
-//     jane owen
-//      = string('can"t do').toBytes();
-//      ad dfd =(((((((string((5.toString())))).add()).def().oi()))));
-//      bool oi = bool(true).concat();
-//      bool cd = (((((true)))));
-// bytes hexs = hex"adfadfad";
-//      sdfd[] public constant jane=[1,2,3,4,5];
-// event Transfer(uind.asd[(((100)))] indexed,address indexed);
+    address yo = chiii({a:1,b:3}).to_val().another();
+    
+    address user = address   (address("sdd").arg().arch(1)).toString().toAddr();
+     bytes user = bytes16(bytes32(0xa).toString(1000_000)).toBytes(16,user).oi();
+     bytes bts = bytes(0x1);
+    jane owen
+     = string('can"t do').toBytes();
+     ad dfd =(((((((string((5)))).add()).def().oi()))));
+     bool oi = bool(true).concat();
+     bool cd = (((((true)))));
+bytes hexs = hex"adfadfad";
+     sdfd[] public constant jane=[1,2,3,4,5];
+event Transfer(uind.asd[(((100)))] indexed,address indexed);
 
-//     uint sdf = uint8(wed).to_bts().add();
+    uint sdf = uint8(wed).to_bts().add();
 
     function oi() {
         {}
