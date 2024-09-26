2c44436cc250f0712b4f7ccb601911d70858c3c1
zarah <devvick230385@gmail.com>
2024-09-26 09:54:15 +0100

    UPDATE: cleaned code

--- src/mods.rs
+++ src/mods.rs
@@ -33,20 +33,6 @@ pub mod ast {
     pub mod variable;
 }
 
-// pub mod parser {
-//     pub mod
-// }
-// pub mod parser {
-//     pub mod custom_error;
-// pub mod r#enum;
-// pub mod event;
-// pub mod functions;
-// pub mod lib_implementation;
-// pub mod mapping;
-// pub mod r#struct;
-// pub mod variable;
-// }
-
 pub mod parser {
     pub mod custom_error;
     pub mod r#enum;
@@ -58,39 +44,6 @@ pub mod parser {
     pub mod variable;
 }
 
-// pub mod types {
-//     pub mod compiler_errors;
-//     // pub mod context;
-//     // pub mod line_descriptors;
-// }
-
 pub mod constants {
     pub mod constants;
-}
-
-pub mod functions {
-    pub mod helpers {
-        // pub mod error_helper;
-        // pub mod global;
-        // pub mod token_helper;
-    }
-
-    pub mod controllers {
-
-        // pub mod process_enum;
-        // pub mod process_file_contents;
-        // pub mod process_function;
-        // pub mod process_state_variables;
-        // pub mod process_struct;
-        // pub mod parser;
-        // pub mod strip_comments;
-        // pub mod structure_to_line_descriptors;
-        // pub mod sub_main;
-    }
-}
-
-pub mod implementations {
-    // pub mod conditionals;
-    // pub mod line_descriptors;
-    // pub mod mapping;
 }
